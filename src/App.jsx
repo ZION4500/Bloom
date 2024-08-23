2fd14796a7461c67f6177ce0398e6f4109b309e3
ZION4500 <143736428+ZION4500@users.noreply.github.com>
2024-08-23 16:18:21 +0100

    auth html layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Posts from "./Pages/Post"
 import Cont from "./Pages/contact"
 import About from "./Pages/about"
 import Error from "./Pages/404"
+import Register from "./components/Register"
 import {BrowserRouter,Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/posts" element={<Posts/>} />
           <Route path="/contact" element={<Cont/>} />
           <Route path="/about" element={<About/>} />
+          <Route path="/login" element={<Register/>} />
           <Route path="*" element={<Error/>} />  
         </Route>
       </Routes>
---
+++ src/components/Register.jsx
@@ -0,0 +1,21 @@
+
+export default function Register() {
+
+    return (
+        <main>
+                <form method="post" className="bg-black" id="register">
+                    <input type="email" name="" id=""  className=" "/>
+                    <input type="password" name="" id="" className=" "/>
+                    <input type="password" name="" id="" className=" "/>
+                    <input type="checkbox" name="" id="" className=""/>
+                </form>
+                <form method="post"id="login">
+                    <input type="email" name="" id="" />
+                    <input type="password" name="" id="" />
+                    <input type="password" name="" id="" />
+                    <input type="checkbox" name="" id="" />
+                </form>
+
+        </main>
+    )
+}
